5e6dae314a28c31617fab1c229180498750c2653
le-mich <dev@le-mi.ch>
2022-09-05 17:04:08 +0200

    markdown lsp

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,6 +1,8 @@
+-- start grammar checker
+require('lspconfig').ltex.setup(require('coq').lsp_ensure_capabilities())
+
 -- 80 column hint
 vim.wo.colorcolumn = vim.wo.colorcolumn .. '80'
-
 
 -- set indentation
 vim.bo.tabstop = 2
