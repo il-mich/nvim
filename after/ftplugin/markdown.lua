a4457bb54f884936c40d562b89ac5a28bc8e0c6e
le-mich <dev@le-mi.ch>
2022-09-07 17:20:24 +0200

    disable ltex spellcheck and enable nvim's built-in one

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,5 +1,23 @@
--- start grammar checker
-require('lspconfig').ltex.setup(require('coq').lsp_ensure_capabilities())
+-- start lsp grammar checker but disable spellchecker
+require('lspconfig').ltex.setup(
+	require('coq').lsp_ensure_capabilities(
+		{
+			settings = {
+				ltex = {
+					disabledRules = {
+						["en"]    = { "MORFOLOGIK_RULE_EN"    },
+						["en-GB"] = { "MORFOLOGIK_RULE_EN_GB" },
+						["en-US"] = { "MORFOLOGIK_RULE_EN_US" },
+						["it"]    = { "MORFOLOGIK_RULE_IT_IT" },
+					},
+				},
+			},
+		}
+	)
+)
+
+-- start nvim spellchecker
+vim.wo.spell = true
 
 -- 80 column hint
 vim.wo.colorcolumn = vim.wo.colorcolumn .. '80'
