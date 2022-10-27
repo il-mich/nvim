93d8b1368f33435ed1db7d6432201f4e429303ff
Michele Ferrante <michele.ferrante@protonmail.ch>
2022-10-27 17:02:54 +0200

    removed external plugins and moved some configuration previously in general

--- lua/style.lua
+++ lua/style.lua
@@ -1,10 +1,22 @@
+-- show line numbers
+vim.o.number = true
+vim.o.relativenumber = true
+
+-- wrap lines
+vim.o.wrap = true
+
+-- use the number column for the text of wrapped lines
+vim.o.cpoptions = vim.o.cpoptions .. 'n'
+
+-- break lines at word
+vim.o.linebreak = true
+
+-- highlight matching brace
+vim.o.showmatch = true
+
+-- line height
+vim.o.linespace = 2
+
+-- remove default bar
 vim.o.showmode = false
-
-require('paq') {
-	'itchyny/lightline.vim'
-}
-
-vim.g.lightline = {
-	colorscheme = 'wombat'
-}
 
